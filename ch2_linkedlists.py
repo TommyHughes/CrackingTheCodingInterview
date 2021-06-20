5c31cef6f35761cd74ec093b58a4761a6f04d0f2
Thomas Hughes <39662708+TommyHughes@users.noreply.github.com>
2021-06-20 16:55:35 -0600

    refactor 2.2 and refactor ch2_linkedlists

--- ch2_linkedlists.py
+++ ch2_linkedlists.py
@@ -1,27 +1,53 @@
 from MyClasses.LinkedList import Node, LinkedList
 from Ch2_LinkedLists.P2_1_RemoveDupes import remove_dupes
+from Ch2_LinkedLists.P2_2_ReturnKthToLast import kth_to_last
 
-# 2.1 Remove Dupes
-ll = LinkedList(Node(10))
-second = Node(55)
-ll.head.next = second
-third = Node(10)
-second.next = third
-fourth = Node(11)
-third.next = fourth
-fifth = Node(1)
-fourth.next = fifth
+def P2_1():
+    # 2.1 Remove Dupes
+    ll = LinkedList(Node(10))
+    second = Node(55)
+    ll.head.next = second
+    third = Node(10)
+    second.next = third
+    fourth = Node(11)
+    third.next = fourth
+    fifth = Node(1)
+    fourth.next = fifth
 
-print('===Linked List Representation===')
-print(ll)
-print('================================')
+    print('===Linked List Representation===')
+    print(ll)
+    print('================================')
 
-print('===FOR LOOP===')
-for node in ll:
-    print(node.value)
-print('==============')
+    print('===FOR LOOP===')
+    for node in ll:
+        print(node.value)
+    print('==============')
 
-print('===NO DUPES===')
-remove_dupes(ll)
-print(ll)
-print('==============')
+    print('===NO DUPES===')
+    remove_dupes(ll)
+    print(ll)
+    print('==============')
+
+def P2_2():
+    #2.2 Kth to Last
+    ll = LinkedList(Node(10))
+    second = Node(55)
+    ll.head.next = second
+    third = Node(10)
+    second.next = third
+    fourth = Node(11)
+    third.next = fourth
+    fifth = Node(1)
+    fourth.next = fifth
+
+    
+    print(kth_to_last(int(input("pick your k ")),ll))
+
+problem_user_wants = input("Which problem output would you like to see (ex P2_1) ")
+
+if problem_user_wants == 'P2_1':
+    P2_1()
+elif problem_user_wants == 'P2_2':
+    P2_2()
+else:
+    print("sorry that problem doesn't exist.")
